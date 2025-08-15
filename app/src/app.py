7ca9ad10fed3b543b4b57567bcc1e75ce0de01c6
Kevin Boyer <kevinboyer@navapbc.com>
2025-08-15 10:33:50 -0400

    Update to proper POST

--- app/src/app.py
+++ app/src/app.py
@@ -5,6 +5,8 @@ import uvicorn
 from hayhooks.settings import settings
 from hayhooks import create_app
 
+from pydantic import BaseModel
+
 
 # Explicitly set the path to the pipelines directory
 os.environ["HAYHOOKS_PIPELINES_DIR"] = "/src/pipelines"
@@ -17,10 +19,13 @@ hayhooks_app = create_app()
 async def health_check():
     return {"status": "ok", "detail": "Hello World!"}
 
+class Greeting(BaseModel):
+    name: str
+
 # route for POST /hello that accepts a name query parameter
 @hayhooks_app.post("/hello")
-async def hello(name: str = "World"):
-    return {"message": f"Hello, {name}!"}
+async def hello(greeting: Greeting):
+    return {"message": f"Hello, {greeting.name}!"}
 
 if __name__ == "__main__":
     uvicorn.run("app:hayhooks_app", host=settings.host, port=settings.port)
