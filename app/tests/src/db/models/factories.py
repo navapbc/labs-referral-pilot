7c304c9e06eb85d648ae03e9398cb4a7e03d5b4a
Michelle Hadfield <michellehadfield@navapbc.com>
2025-09-03 14:58:22 -0600

    copilot is not my friend

--- app/tests/src/db/models/factories.py
+++ app/tests/src/db/models/factories.py
@@ -100,7 +100,7 @@ class SupportFactory(BaseFactory):
     class Meta:
         model = support_listing.Support
 
-    )
+    support_listing = factory.SubFactory(SupportListingFactory)
 
     name = factory.Faker("name")
     addresses = factory.Faker("address")
