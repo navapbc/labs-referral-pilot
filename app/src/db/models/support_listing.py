6fab87c52830c05a18915f9d8526cea1701a154a
Michelle Hadfield <michellehadfield@navapbc.com>
2025-09-03 09:53:52 -0600

    updated comments and removed unnecessary unique=false

--- app/src/db/models/support_listing.py
+++ app/src/db/models/support_listing.py
@@ -11,10 +11,9 @@ class SupportListing(Base, IdMixin, TimestampMixin):
     __tablename__ = "support_listing"
 
     name: Mapped[str] = mapped_column(
-        nullable=False, unique=True, comment="name of the support listing; a human-use identifier"
+        nullable=False, unique=True, comment="name of the support listing; a human-readable identifier"
     )
     uri: Mapped[str] = mapped_column(
         nullable=False,
-        unique=False,
         comment="origin of the Support Listing; a file path or a website URL",
     )
