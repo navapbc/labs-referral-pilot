9d1ca882aef3fd46544d2171ff96e16062c49c97
Kevin Boyer <kevinboyer@navapbc.com>
2025-09-24 11:40:08 -0400

    Minimize diff

--- app/tests/src/pipelines/generate_referrals/test_generate_referrals_pipeline.py
+++ app/tests/src/pipelines/generate_referrals/test_generate_referrals_pipeline.py
@@ -1,33 +1,17 @@
-<<<<<<< Updated upstream
 import pytest
 
 from src.adapters import db
 from src.db.models.support_listing import Support
 from src.pipelines.generate_referrals.pipeline_wrapper import format_support_strings
-=======
-import json
-
-import pytest
-from sqlalchemy.inspection import inspect
-
-from src.adapters import db
-from src.db.models.support_listing import Support
-from src.pipelines.generate_referrals.pipeline_wrapper import retrieve_supports_from_db
->>>>>>> Stashed changes
 from tests.src.db.models.factories import SupportFactory, SupportListingFactory
 
 
 @pytest.fixture
-<<<<<<< Updated upstream
 def seed_supports(enable_factory_create, db_session: db.Session):
-=======
-def seed_supports(db_session: db.Session):
->>>>>>> Stashed changes
     # remove all pre-existing Support records
     db_session.query(Support).delete()
 
     support_listing = SupportListingFactory.create()
-<<<<<<< Updated upstream
     return [SupportFactory.create(support_listing=support_listing) for _ in range(3)]
 
 
@@ -49,23 +33,3 @@ def test_format_support_strings(seed_supports):
         assert support.website in v
         for email_address in support.email_addresses:
             assert email_address in v
-=======
-    supports = []
-    for i in range(0, 3):
-        support = SupportFactory.create(support_listing=support_listing, name=f"support{i}")
-        support_as_json_str = json.dumps(
-            {c.key: getattr(support, c.key) for c in inspect(Support).mapper.column_attrs},
-            default=str,
-        )
-        supports.append(support_as_json_str)
-    return supports
-
-
-def test_retrieve_supports_from_db(enable_factory_create, seed_supports, db_session: db.Session):
-    supports_from_db = retrieve_supports_from_db()
-
-    assert len(supports_from_db) == 3
-    assert seed_supports[0] == supports_from_db[0]
-    assert seed_supports[1] == supports_from_db[1]
-    assert seed_supports[2] == supports_from_db[2]
->>>>>>> Stashed changes
