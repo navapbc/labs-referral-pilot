3b3c73122c93d6238a4618cc9845b09eaecf20e7
Michelle Hadfield <michellehadfield@navapbc.com>
2025-09-24 12:02:08 -0600

    merged external changes from main

--- frontend/src/app/[locale]/generate-referrals/page.tsx
+++ frontend/src/app/[locale]/generate-referrals/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import {Resource} from "@/types/resources";
 
@@ -10,6 +9,7 @@ import {Sparkles} from "lucide-react";
 import "@/app/globals.css"
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import Link from "next/link";
+import {Label} from "@/components/ui/label";
 
 
 export default function Page() {
