96f30a04e5c1af309fa29828f1c6e8e2e4b4c9da
Michelle Hadfield <michellehadfield@navapbc.com>
2025-09-23 18:49:29 -0600

    trying to fix ShadCN not working in DEV

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
   // Output only the necessary files for a deployment, excluding irrelevant node_modules
   // https://nextjs.org/docs/app/api-reference/next-config-js/output
   output: "standalone",
-  transpilePackages: [],
+  transpilePackages: ["@tailwind/postcss"],
 };
 
 module.exports = withNextIntl(nextConfig);
