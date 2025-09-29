f54b4ef2ca211373426fdc3c2b800b8e0f650f02
Michelle Hadfield <michellehadfield@navapbc.com>
2025-09-29 08:25:47 -0600

    moved resources into their own type file

--- frontend/src/util/fetchResources.ts
+++ frontend/src/util/fetchResources.ts
@@ -1,21 +1,4 @@
-import { z } from "zod";
-
-export const ResourceSchema = z.object({
-  name: z.string(),
-  addresses: z.array(z.string()),
-  phones: z.array(z.string()),
-  emails: z.array(z.string().email()),
-  website: z.string().url(),
-  description: z.string(),
-  justification: z.string(),
-});
-
-export const ResourcesSchema = z.object({
-  resources: z.array(ResourceSchema),
-});
-
-export type Resource = z.infer<typeof ResourceSchema>;
-export type Resources = z.infer<typeof ResourcesSchema>;
+import { Resource, ResourcesSchema } from "@/types/resources";
 
 // TODO update the logic above to be ready for when we deploy to a PROD env
 const generateReferralsURL =
