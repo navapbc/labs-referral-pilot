8bba9873a9b0f91d3e4d3fc8d1e396bdecf1660b
Michelle Hadfield <michellehadfield@navapbc.com>
2025-09-30 13:43:54 -0600

    removed url validation for website field because it expects http or https as a prefix

--- frontend/src/types/resources.ts
+++ frontend/src/types/resources.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const ResourceSchema = z.object({
-  name: z.string(),
-  addresses: z.array(z.string()).optional(),
-  phones: z.array(z.string()).optional(),
-  emails: z.array(z.string().email()).optional(),
-  website: z.string().url().optional().or(z.literal("")),
+  addresses: z.array(z.string().optional()).optional(),
   description: z.string().optional(),
+  emails: z.array(z.string().email().optional()).optional(),
   justification: z.string().optional(),
+  name: z.string(),
+  phones: z.array(z.string().optional()).optional(),
+  website: z.string().optional().or(z.literal("")),
 });
 
 export const ResourcesSchema = z.object({
--- frontend/tests/test-resource-type.test.ts
+++ frontend/tests/test-resource-type.test.ts
@@ -2,6 +2,25 @@
 import { ResourceSchema } from "@/types/resources";
 
 describe("ResourceSchema", () => {
+  it("parses ok when all conditions met", () => {
+    const resourceWithEverythingOK = {
+      name: "Capital Metro",
+      addresses: ["Austin, Texas"],
+      phones: ["512-474-1200", "1-800-474-1201"],
+      emails: ["info@capmetro.org", "capital@metro.org"],
+      website: "http://www.capitalmetro.org",
+      description:
+        "Public transportation service provider in Austin. Offers route planning assistance and transit information services.",
+      justification:
+        "Can help plan reliable transportation routes to/from work using public transit system.",
+    };
+
+    const result = ResourceSchema.safeParse(resourceWithEverythingOK);
+    expect(result.success).toBe(true);
+    expect(result?.data?.phones?.length).toBe(2);
+    expect(result?.data?.emails?.[0]).toBe("info@capmetro.org");
+  });
+
   it("parses a resource with empty emails and empty website", () => {
     const resourceWithoutEmailsOrWebsite = {
       name: "Capital Metro",
@@ -42,16 +61,6 @@ describe("ResourceSchema", () => {
     expect(result?.data?.website).toBeUndefined(); // missing field is fine
   });
 
-  it("fails when website is not a valid URL and not empty string", () => {
-    const resourceWithInvalidWebsite = {
-      name: "Capital Metro",
-      website: "not-a-url",
-    };
-
-    const result = ResourceSchema.safeParse(resourceWithInvalidWebsite);
-    expect(result.success).toBe(false);
-  });
-
   it("parses when emails are omitted", () => {
     const resourceWithoutEmails = {
       name: "Capital Metro",
@@ -85,8 +94,8 @@ describe("ResourceSchema", () => {
     expect(result.success).toBe(true);
     expect(result?.data?.emails?.length).toBe(2);
     expect(result?.data?.emails?.[0]).toBe("info@capmetro.org");
-
   });
+
   it("fails when name is missing", () => {
     const resourceWithNoName = {
       addresses: ["Austin, Texas"],
@@ -100,24 +109,5 @@ describe("ResourceSchema", () => {
 
     const result = ResourceSchema.safeParse(resourceWithNoName);
     expect(result.success).toBe(false);
-  });
-
-  it("parses when all conditions met", () => {
-    const resourceWithEverythingOK = {
-      name: "Capital Metro",
-      addresses: ["Austin, Texas"],
-      phones: ["512-474-1200", "1-800-474-1201"],
-      emails: ["info@capmetro.org", "capital@metro.org"],
-      website: "http://www.capitalmetro.org",
-      description:
-        "Public transportation service provider in Austin. Offers route planning assistance and transit information services.",
-      justification:
-        "Can help plan reliable transportation routes to/from work using public transit system.",
-    };
-
-    const result = ResourceSchema.safeParse(resourceWithEverythingOK);
-    expect(result.success).toBe(true);
-    expect(result?.data?.phones?.length).toBe(2);
-    expect(result?.data?.emails?.[0]).toBe("info@capmetro.org");
   });
 });
