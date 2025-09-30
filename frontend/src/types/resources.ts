336a61f3e5abf6f4cca6ed07f4cfb48000163b02
Michelle Hadfield <michellehadfield@navapbc.com>
2025-09-30 12:58:42 -0600

    changed resource type so that some fields could be optional and added unit tests

--- frontend/src/types/resources.ts
+++ frontend/src/types/resources.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 
 export const ResourceSchema = z.object({
   name: z.string(),
-  addresses: z.array(z.string()),
-  phones: z.array(z.string()),
-  emails: z.array(z.string().email()),
-  website: z.string().url(),
-  description: z.string(),
-  justification: z.string(),
+  addresses: z.array(z.string()).optional(),
+  phones: z.array(z.string()).optional(),
+  emails: z.array(z.string().email()).optional(),
+  website: z.string().url().optional().or(z.literal("")),
+  description: z.string().optional(),
+  justification: z.string().optional(),
 });
 
 export const ResourcesSchema = z.object({
