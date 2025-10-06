1ca50609958f02d30f5423e6e56550393add1095
Michelle Hadfield <michellehadfield@navapbc.com>
2025-10-06 16:47:00 -0600

    added CSP to middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -5,7 +5,7 @@
  * @see https://nextjs.org/docs/app/building-your-application/routing/middleware
  */
 import createIntlMiddleware from "next-intl/middleware";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
 import { defaultLocale, locales } from "./i18n/config";
 
@@ -25,7 +25,38 @@ const i18nMiddleware = createIntlMiddleware({
   localePrefix: "as-needed",
 });
 
-export default function middleware(req: NextRequest) {
-  const response = i18nMiddleware(req);
-  return response;
+export default function middleware(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, ' ')
+    .trim()
+
+  const response = i18nMiddleware(request);
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+
+  requestHeaders.set(
+    'Content-Security-Policy',
+    contentSecurityPolicyHeaderValue
+  )
+
+  response.headers.set(
+    'Content-Security-Policy',
+    contentSecurityPolicyHeaderValue
+  )
+
+  return response
 }
