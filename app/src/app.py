511db68a9beb92c211007ccaae39e1a01bff5eae
Kevin Boyer <kevinboyer@navapbc.com>
2025-10-16 14:20:34 -0400

    chore: Add logging to start up and /health (#45)

--- app/src/app.py
+++ app/src/app.py
@@ -7,12 +7,22 @@ from src.common import phoenix_utils
 
 logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.INFO)
 
+logging.info("Configuring Phoenix...")
+
 phoenix_utils.configure_phoenix()
 
+logging.info("Phoenix configured.")
+
 # Boot the standard Hayhooks app
+
+logging.info("Starting Hayhooks app...")
+
 hayhooks_app = create_app()
+
+logging.info("Hayhooks app started.")
 
 
 @hayhooks_app.get("/health")
 async def health_check() -> Dict[str, str]:
+    logging.info("Health check returning OK.")
     return {"status": "ok", "detail": "Hello World!"}
