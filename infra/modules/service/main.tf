65ae3dadb9d6f76574e714e1375cf021dbe955dd
Kevin Boyer <kevinboyer@navapbc.com>
2025-10-16 14:45:36 -0400

    Update

--- infra/modules/service/main.tf
+++ infra/modules/service/main.tf
@@ -55,12 +55,17 @@ locals {
 #-------------------
 
 resource "aws_ecs_service" "app" {
-  name                   = var.service_name
-  cluster                = aws_ecs_cluster.cluster.arn
-  launch_type            = "FARGATE"
-  task_definition        = aws_ecs_task_definition.app.arn
-  desired_count          = var.desired_instance_count
-  enable_execute_command = var.enable_command_execution ? true : null
+  name                              = var.service_name
+  cluster                           = aws_ecs_cluster.cluster.arn
+  launch_type                       = "FARGATE"
+  task_definition                   = aws_ecs_task_definition.app.arn
+  desired_count                     = var.desired_instance_count
+  enable_execute_command            = var.enable_command_execution ? true : null
+
+
+  # Our application can take some time to boot up, so don't let ECS shut it down prematurely
+  # for failing health checks
+  health_check_grace_period_seconds = 300
 
   # Allow changes to the desired_count without differences in terraform plan.
   # This allows autoscaling to manage the desired count for us.
