7d013f084c4a517051cef440c248713135e68e44
Yoom Lam <yoom@navapbc.com>
2025-12-03 17:05:56 -0600

    add gunicorn_config.py

--- app/src/app.py
+++ app/src/app.py
@@ -5,7 +5,6 @@ from hayhooks import create_app
 
 from src.app_config import config
 from src.common import phoenix_utils
-from src.ingestion import rag_utils
 
 logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.INFO)
 
@@ -13,16 +12,10 @@ logging.info("Configuring Phoenix...")
 phoenix_utils.configure_phoenix()
 logging.info("Phoenix configured.")
 
-logging.info("Ingesting into ChromaDB...")
-rag_utils.populate_vector_db()
-logging.info("ChromaDB populated. Collections: %s", config.chroma_client().list_collections())
-
 # Boot the standard Hayhooks app
 
 logging.info("Starting Hayhooks app...")
-
 hayhooks_app = create_app()
-
 logging.info("Hayhooks app started.")
 
 
