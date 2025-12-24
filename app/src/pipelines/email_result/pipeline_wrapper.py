aefc6219457d93b9b9eea50d7b5523e9c2b087a6
Michelle Hadfield <michellehadfield@navapbc.com>
2025-12-23 19:06:16 -0700

    updated pipeline for consistency

--- app/src/pipelines/email_result/pipeline_wrapper.py
+++ app/src/pipelines/email_result/pipeline_wrapper.py
@@ -28,15 +28,15 @@ class PipelineWrapper(BasePipelineWrapper):
 
         self.pipeline = pipeline
 
-    def run_api(self, result_id: str, email: str) -> dict:
+    def run_api(self, resources_result_id: str, email: str) -> dict:
         with using_metadata({"email": email}):
             # Must set using_metadata context before calling tracer.start_as_current_span()
             assert isinstance(tracer, _tracers.OITracer), f"Got unexpected {type(tracer)}"
             with tracer.start_as_current_span(  # pylint: disable=not-context-manager,unexpected-keyword-arg
                 self.name, openinference_span_kind="chain"
             ) as span:
-                result = self._run(result_id, email)
-                span.set_input(result_id)
+                result = self._run(resources_result_id, email)
+                span.set_input(resources_result_id)
                 span.set_output(result["email_result"]["status"])
                 span.set_status(Status(StatusCode.OK))
                 return result
