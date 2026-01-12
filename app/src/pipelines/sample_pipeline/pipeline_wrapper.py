2e0cc86ec6a66f610bf4e6be8d58e3706c46ec12
Yoom Lam <yoom@navapbc.com>
2026-01-12 12:49:00 -0600

    feat: Enable streaming in sample_pipeline (#138)

--- app/src/pipelines/sample_pipeline/pipeline_wrapper.py
+++ app/src/pipelines/sample_pipeline/pipeline_wrapper.py
@@ -1,14 +1,15 @@
 import logging
 from pprint import pformat
+from typing import Generator
 
 import hayhooks
 from hayhooks import BasePipelineWrapper
 from haystack import Pipeline
+from haystack.components.builders import ChatPromptBuilder
+from haystack.components.generators.chat import OpenAIChatGenerator
 from haystack.dataclasses.chat_message import ChatMessage
-from openinference.instrumentation import _tracers, using_metadata
-from opentelemetry.trace.status import Status, StatusCode
 
-from src.common import components, phoenix_utils
+from src.common import components, haystack_utils, phoenix_utils
 
 logger = logging.getLogger(__name__)
 tracer = phoenix_utils.tracer_provider.get_tracer(__name__)
@@ -20,59 +21,78 @@ class PipelineWrapper(BasePipelineWrapper):
     name = "sample_pipeline"
 
     def setup(self) -> None:
+        prompt_builder = ChatPromptBuilder()
+        llm = OpenAIChatGenerator(model="gpt-4o-mini")
+
         self.pipeline = Pipeline()
         self.pipeline.add_component("echo_component", components.EchoNode())
         self.pipeline.add_component("echo_component2", components.EchoNode())
         self.pipeline.add_component("logger", components.ReadableLogger())
 
+        self.pipeline.add_component("prompt_builder", prompt_builder)
+        self.pipeline.add_component("llm", llm)
+
         self.pipeline.connect("echo_component", "logger")
         self.pipeline.connect("echo_component2", "logger")
+        self.pipeline.connect("prompt_builder.prompt", "llm.messages")
+        self.pipeline.connect("prompt_builder.prompt", "logger")
+        self.pipeline.connect("llm", "logger")
+
+        self.runner = haystack_utils.TracedPipelineRunner(self.name, self.pipeline)
 
     # Called for the `sample_pipeline/run` endpoint
     def run_api(self, question: str) -> dict:
-        with using_metadata({"user_id": "someone@example.com"}):
-            # Must set using_metadata context before calling tracer.start_as_current_span()
-            assert isinstance(tracer, _tracers.OITracer), f"Got unexpected {type(tracer)}"
-            with tracer.start_as_current_span(  # pylint: disable=not-context-manager,unexpected-keyword-arg
-                self.name, openinference_span_kind="chain"
-            ) as span:
-                result = self._run(question)
-                # Shorter than span.set_attribute(SpanAttributes.INPUT_VALUE, question)
-                span.set_input(question)
-                span.set_output(result["echo_component"]["full_prompt"][-1].texts)
-                span.set_status(Status(StatusCode.OK))
-                return result
-
-    def _run(self, question: str) -> dict:
+        user_id = "someone@example.com"
         messages = [
             ChatMessage.from_system(system_msg),
             ChatMessage.from_user(question),
         ]
-        response = self.pipeline.run(
-            {
-                "logger": {
-                    "messages_list": [{"question": question}],
-                },
-                "echo_component": {"prompt": messages, "history": []},
-                "echo_component2": {"prompt": messages, "history": []},
+        location = "Berlin"
+        pipeline_run_args = self.create_pipeline_args(location, messages) | {
+            "logger": {
+                "messages_list": [{"question": question}],
             },
+            "echo_component": {"prompt": messages, "history": []},
+        }
+        response = self.runner.return_response(
+            pipeline_run_args,
+            metadata={"user_id": user_id},
             include_outputs_from={"echo_component", "echo_component2"},
+            input_=question,
+            extract_output=lambda result: result["echo_component"]["full_prompt"][-1].texts,
         )
         logger.info("Results: %s", pformat(response))
         return response
 
     # https://docs.haystack.deepset.ai/docs/hayhooks#openai-compatibility
     # Called for the `{pipeline_name}/chat`, `/chat/completions`, or `/v1/chat/completions` streaming endpoint using Server-Sent Events (SSE)
-    def run_chat_completion(self, model: str, messages: list, body: dict) -> None:
-        logger.info("Running chat completion with model: %s, messages: %s", model, messages)
+    def run_chat_completion(self, model: str, messages: list, body: dict) -> Generator:
+        logger.info("Running streaming LLM with model: %s, messages: %s", model, messages)
         question = hayhooks.get_last_user_message(messages)
         logger.info("Question: %s", question)
-        return hayhooks.streaming_generator(
-            pipeline=self.pipeline,
-            pipeline_run_args={
-                "echo_component": {
-                    "prompt": [ChatMessage.from_user(question)],
-                    "history": messages[:-1],
-                }
+
+        location = "Berlin"
+        chat_messages = haystack_utils.to_chat_messages(messages)
+        chat_messages.append(ChatMessage.from_user("Write a summary sentence about {{location}}"))
+
+        pipeline_run_args = self.create_pipeline_args(location, chat_messages) | {
+            "logger": {
+                "messages_list": chat_messages,
             },
+            "echo_component": {
+                "prompt": [ChatMessage.from_user(question)],
+                "history": chat_messages[:-1],
+            },
+        }
+
+        user_id = "someone@example.com"
+        return self.runner.stream_response(
+            pipeline_run_args, metadata={"user_id": user_id}, input_=question
         )
+
+    def create_pipeline_args(self, location: str, messages: list[ChatMessage]) -> dict:
+        "Common args for both run_api and run_chat_completion"
+        return {
+            "echo_component2": {"prompt": messages, "history": []},
+            "prompt_builder": {"template_variables": {"location": location}, "template": messages},
+        }
