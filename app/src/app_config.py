e5e82913478e0ae6ac82cacbe4366eb82d1c129b
Michelle Hadfield <116575227+MichelleHadfield@users.noreply.github.com>
2026-03-04 18:23:50 -0700

    fix: updated prompt version to refer to childcare specific instructions (#202)

--- app/src/app_config.py
+++ app/src/app_config.py
@@ -44,8 +44,8 @@ class AppConfig(PydanticBaseEnvConfig):
     # Be aware: Version ids are base64 encodings of 'PromptVersion:N' where N is simply a counter,
     # so they are not unique across different Phoenix instances.
     PROMPT_VERSIONS: dict = {
-        "generate_referrals_centraltx": "UHJvbXB0VmVyc2lvbjoxNDM=",
-        "generate_referrals_keystone": "UHJvbXB0VmVyc2lvbjo3OQ==",
+        "generate_referrals_centraltx": "UHJvbXB0VmVyc2lvbjoxNjU=",
+        "generate_referrals_keystone": "UHJvbXB0VmVyc2lvbjoxNjQ=",
         "generate_action_plan": "UHJvbXB0VmVyc2lvbjoxNTk=",
     }
 
